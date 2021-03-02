9b3c186f247fab4d806b2491317c603667185707
Makhmut <mahmudlyder@gmail.com>
2021-03-03 03:28:34 +0600

    2nd commit

--- src/Assignments/Week9/Factory.java
+++ src/Assignments/Week9/Factory.java
@@ -1,128 +1,41 @@
 package Assignments.Week9;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-interface Button {
-    void render();
-    void onClick();
+interface Transport{
+    void deliver();
 }
-
-class HtmlButton implements Button {
-
-    public void render() {
-        System.out.println("<button>Test Button</button>");
-        onClick();
-    }
-
-    public void onClick() {
-        System.out.println("Click! Button says - 'Hello World!'");
+class Truck implements Transport{
+    public void deliver(){
+        System.out.println("Deliver by land in a box");
     }
 }
-
-
-
-/**
- * Windows button implementation.
- */
-class WindowsButton implements Button {
-    JPanel panel = new JPanel();
-    JFrame frame = new JFrame();
-    JButton button;
-
-    public void render() {
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel label = new JLabel("Hello World!");
-        label.setOpaque(true);
-        label.setBackground(new Color(235, 233, 126));
-        label.setFont(new Font("Dialog", Font.BOLD, 44));
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-        panel.setLayout(new FlowLayout(FlowLayout.CENTER));
-        frame.getContentPane().add(panel);
-        panel.add(label);
-        onClick();
-        panel.add(button);
-
-        frame.setSize(320, 200);
-        frame.setVisible(true);
-        onClick();
-    }
-
-    public void onClick() {
-        button = new JButton("Exit");
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                frame.setVisible(false);
-                System.exit(0);
-            }
-        });
+class Ship implements Transport{
+    public void deliver(){
+        System.out.println("Deliver by sea in a box");
     }
 }
-
- abstract class Dialog {
-
-    public void renderWindow() {
-        // ... other code ...
-
-        Button okButton = createButton();
-        okButton.render();
+abstract class Logistics{
+    Transport t;
+    public void planDeliver(){
+        t=createTransport();
     }
-
-    /**
-     * Subclasses will override this method in order to create specific button
-     * objects.
-     */
-    public abstract Button createButton();
+    public abstract Transport createTransport();
 }
-
- class HtmlDialog extends Dialog {
-
+class RoadLogistics extends Logistics{
     @Override
-    public Button createButton() {
-        return new HtmlButton();
+    public Transport createTransport(){
+        return new Truck();
     }
 }
-
- class WindowsDialog extends Dialog {
-
+class SeaLogistics extends Logistics{
     @Override
-    public Button createButton() {
-        return new WindowsButton();
+    public Transport createTransport(){
+        return new Ship();
     }
 }
-
-public class Factory {
-    private static Dialog dialog;
-
+public class Factory{
     public static void main(String[] args) {
-        configure();
-        runBusinessLogic();
-    }
-
-    /**
-     * The concrete factory is usually chosen depending on configuration or
-     * environment options.
-     */
-    static void configure() {
-        if (System.getProperty("os.name").equals("Windows 10")) {
-            dialog = new WindowsDialog();
-        } else {
-            dialog = new HtmlDialog();
-        }
-    }
-
-    /**
-     * All of the client code should work with factories and products through
-     * abstract interfaces. This way it does not care which factory it works
-     * with and what kind of product it returns.
-     */
-    static void runBusinessLogic() {
-        dialog.renderWindow();
+        Logistics post=new RoadLogistics();
+        post.planDeliver();
+        post.t.deliver();
     }
 }
-
-
-
-
